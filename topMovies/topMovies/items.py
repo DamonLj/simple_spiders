a5ed369cf8abd22b8dbf7124e7aa668f4f6b28b9
ljmgps <ljmgps@163.com>
2017-07-17 09:39:36 +0800

    first version

---
+++ topMovies/topMovies/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TopmoviesItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    name = scrapy.Field() # 电影名字
+    num = scrapy.Field() # 电影排名
+    year = scrapy.Field() # 电影年份
+    area = scrapy.Field() # 电影地区
+    style = scrapy.Field() # 电影类型
---
+++ topMovies/topMovies/spiders/dbtopMovies.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from topMovies.items import TopmoviesItem
+
+from bs4 import BeautifulSoup
+import lxml
+
+
+class DbtopmoviesSpider(scrapy.Spider):
+    name = "dbtopMovies"
+    allowed_domains = ["douban.com"]
+    # start_urls = ['https://movie.douban.com/top250?start=' + str(x) + '&filter=' for x in range(0, 226, 25)]
+    # start_urls = ['https://movie.douban.com/top250?start=25&filter='] 豆瓣屏蔽爬虫 返回403错误
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+    }
+
+    def start_requests(self): # 以新的的headers提交初始Request
+        urls = ['https://movie.douban.com/top250?start=' + str(x) + '&filter=' for x in range(0, 226, 25)]
+        for url in urls:
+            yield scrapy.Request(url, headers=self.headers)
+
+    def parse(self, response):
+        movie = TopmoviesItem()
+        soup = BeautifulSoup(response.body.decode(response.encoding), 'lxml')
+        ol = soup.ol
+        for li in ol('li'):
+            movie['name'] = li('span')[0].string
+            movie['num'] = li('em')[0].string
+            info = li('div', class_='bd')[0].p.contents[-1].split('\xa0')
+            movie['year'] = info[0].strip()
+            movie['area'] = info[2]
+            movie['style'] = info[-1].strip()
+            yield movie
