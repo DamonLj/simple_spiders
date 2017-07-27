25d308e849c1f637c31e89829dadac2729153289
ljmgps <ljmgps@163.com>
2017-07-27 13:41:34 +0800

    crawl gintama2/3

--- gintama/gintama/spiders/dilidili.py
+++ gintama/gintama/spiders/dilidili.py
@@ -2,6 +2,7 @@
 import scrapy
 from gintama.items import GintamaItem
 
+
 class DilidiliSpider(scrapy.Spider):
     name = 'dilidili'
     allowed_domains = ['dilidili.wang']
@@ -11,9 +12,23 @@ class DilidiliSpider(scrapy.Spider):
         gintamalist = response.xpath("//div[@class='series area']//tr")[1:]
         gintama = GintamaItem()
         for tr in gintamalist:
-            gintama['episode'] = tr.xpath('td[1]/text()').extract()[0]
-            gintama['gintamatitle'] = tr.xpath('td[2]/a/text()').extract()[0]
-            yield gintama
-        nexturls = response.xpath("//div[@id='tab-menu']/h2/a[@style='color:#333;']/@href").extract()
-        for url in nexturls:
-            yield scrapy.Request(url, callback='parse')
+            try:
+                gintama['episode'] = tr.xpath('td[1]/text()')[0].re(r'\d+')[0]
+                gintama['gintamatitle'] = tr.xpath('td[2]/a/text()')[0].extract()
+                yield gintama
+            except:
+                continue
+        next_urls = response.xpath("//div[@id='tab-menu']/h2/a[@style='color:#333;']/@href").extract()
+        for url in next_urls:
+            yield scrapy.Request(url, callback=self.parse_2)
+
+    def parse_2(self, response):
+        gintamalist = response.xpath("//div[@class='series area']//tr")[1:]
+        gintama = GintamaItem()
+        for tr in gintamalist:
+            try:
+                gintama['episode'] = tr.xpath('td[1]/text()')[0].re(r'\d+')[0]
+                gintama['gintamatitle'] = tr.xpath('td[2]/a/text()').extract()[0]
+                yield gintama
+            except:
+                continue
