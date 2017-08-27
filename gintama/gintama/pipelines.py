729edf55221cde03b4e0f9b9fe50ec8b2a86bf17
DamonLj <ljdamon@163.com>
2017-08-27 23:56:52 +0800

    output with json

--- gintama/gintama/pipelines.py
+++ gintama/gintama/pipelines.py
@@ -5,17 +5,22 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+import codecs
+from collections import OrderedDict
 
 class GintamaPipeline(object):
     def open_spider(self, spider):
-        self.f = open('gintama.txt', 'w', encoding='utf8')
+        print("GintamaPipeline opened")
+        self.f = codecs.open('gintama.json', 'w', encoding='utf-8')
 
     def close_spider(self, spider):
+        print("GintamaPipeline closed")
         self.f.close()
 
     def process_item(self, item, spider):
         try:
-            line = str(dict(item)) + '\n'
+            line = json.dumps(OrderedDict(item), ensure_ascii=False, sort_keys=False) + '\n'
             self.f.write(line)
         except:
             pass
