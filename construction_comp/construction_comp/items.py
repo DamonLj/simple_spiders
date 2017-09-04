d298b1fb2250ecb59e04fa7d51dc2e20e1447c90
DamonLj <ljdamon@163.com>
2017-09-05 00:10:12 +0800

    comp['name']

--- construction_comp/construction_comp/items.py
+++ construction_comp/construction_comp/items.py
@@ -11,4 +11,7 @@ import scrapy
 class ConstructionCompItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    adress = scrapy.Field()
+    id = scrapy.Field()
+    Certifications = scrapy.Field()
--- construction_comp/construction_comp/spiders/jzsc.py
+++ construction_comp/construction_comp/spiders/jzsc.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from bs4 import BeautifulSoup
+import lxml
+
+from construction_comp.items import ConstructionCompItem
 
 
 class JzscSpider(scrapy.Spider):
     name = "jzsc"
-    allowed_domains = ["http://jzsc.mohurd.gov.cn/dataservice/query/comp/list"]
+    # allowed_domains = ["http://jzsc.mohurd.gov.cn"]
 
     def start_requests(self):
         requestlist = []
-        for i in range(5):
+        for i in range(1):
             # formdata的value必须是string!
             requestlist.append(scrapy.FormRequest(r'http://jzsc.mohurd.gov.cn/dataservice/query/comp/list',
-                                                  formdata={'$pg': str(i+1)}, callback=self.parse_complist,
+                                                  formdata={'$pg': str(i+1)}, callback=self.parse_urllist,
                                                   method='POST'))
         return requestlist
 
-    def parse_complist(self, response):
-        comp_namelist = response.xpath("//td[@data-header='企业名称']/a/text()").extract()
-        comp_namelist = list(map(lambda x: x.strip(), comp_namelist))
-        print(comp_namelist)
+    def parse_urllist(self, response):
+        comp_hreflist = response.xpath("//td[@data-header='企业名称']/a/@href").extract()
+        for href in comp_hreflist:
+            url = r'http://jzsc.mohurd.gov.cn' + href
+            yield scrapy.Request(url, callback=self.parse_comp)
+
+    def parse_comp(self, response):
+        soup = BeautifulSoup(response.text, 'lxml')
+        comp = ConstructionCompItem()
+        comp['name'] = soup('div', class_='user_info spmtop')[0].b.contents[-1].strip()
+        # comp['name'] = response.xpath("//div[@class='user_info spmtop']/b/text()").extract()[0].strip()
+        comp['id'] = soup('table', class_='pro_table_box datas_table')
