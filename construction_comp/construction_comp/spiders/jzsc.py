516c72ac34a4c603ec6f044bcb1cdf5bb3d0f2fc
ljmgps <ljmgps@163.com>
2017-11-07 14:35:08 +0800

    only no members have exception

--- construction_comp/construction_comp/spiders/jzsc.py
+++ construction_comp/construction_comp/spiders/jzsc.py
@@ -78,22 +78,26 @@ class JzscSpider(scrapy.Spider):
             page_str = pagebar[-1].extract()
             import re
             total = int(re.search(r'tt:(\d+),', page_str).group(1))
-            for p in range(total // self.PAGESIZE):
-                yield scrapy.FormRequest(response.url,
-                                         formdata={'$pg': str(p + 1)}, callback=self.parse_members_none,
-                                         method='POST', headers=self.headers, meta={'item': comp}, priority=-3)
-            yield scrapy.FormRequest(response.url,
-                                     formdata={'$pg': str(total // self.PAGESIZE + 1)}, callback=self.parse_members,
-                                     method='POST', headers=self.headers, meta={'item': comp}, priority=-4)
+            pagesize = int(re.search(r'ps:(\d+),', page_str).group(1))
+            remainder = total % pagesize
+            page_num = total // pagesize + 1
+            for p in range(page_num - 1):
+                yield scrapy.FormRequest(response.url, formdata={'$pg': str(p + 1)}, callback=self.parse_members_none,
+                                         method='POST', headers=self.headers,
+                                         meta={'item': comp, 'pagesize': pagesize}, priority=-3)
+            yield scrapy.FormRequest(response.url, formdata={'$pg': str(page_num)},
+                                     callback=self.parse_members_last, method='POST', headers=self.headers,
+                                     meta={'item': comp, 'remainder': remainder}, priority=-4)
 
-    def parse_members(self, response):
+    def parse_members_last(self, response):
         comp = response.meta['item']
+        remainder = response.meta['remainder']
         if 'members' not in comp:
             comp['members'] = {}
         soup = BeautifulSoup(response.text, 'lxml')
         try:
             allmembers = soup('table', class_='pro_table_box pro_table_borderright')[0]('tbody')[0]('tr')
-            for member in allmembers[:self.PAGESIZE]:
+            for member in allmembers[:remainder]:
                 member_cat = member('td', attrs={'data-header': '注册类别'})[0].string.strip()
                 member_name = member('td', attrs={'data-header': '姓名'})[0].a.string.strip()
                 if member_cat in comp['members']:
@@ -107,12 +111,13 @@ class JzscSpider(scrapy.Spider):
 
     def parse_members_none(self, response):
         comp = response.meta['item']
+        pagesize = response.meta['pagesize']
         if 'members' not in comp:
             comp['members'] = {}
         soup = BeautifulSoup(response.text, 'lxml')
         try:
             allmembers = soup('table', class_='pro_table_box pro_table_borderright')[0]('tbody')[0]('tr')
-            for member in allmembers[:self.PAGESIZE]:
+            for member in allmembers[:pagesize]:
                 member_cat = member('td', attrs={'data-header': '注册类别'})[0].string.strip()
                 member_name = member('td', attrs={'data-header': '姓名'})[0].a.string.strip()
                 if member_cat in comp['members']:
@@ -122,3 +127,22 @@ class JzscSpider(scrapy.Spider):
                     comp['members'][member_cat].append(member_name)
         except:
             print(response.url, '产生异常！！！未找到member。')
+
+    def parse_members(self, response):
+        comp = response.meta['item']
+        if 'members' not in comp:
+            comp['members'] = {}
+        soup = BeautifulSoup(response.text, 'lxml')
+        try:
+            allmembers = soup('table', class_='pro_table_box pro_table_borderright')[0]('tbody')[0]('tr')
+            for member in allmembers:
+                member_cat = member('td', attrs={'data-header': '注册类别'})[0].string.strip()
+                member_name = member('td', attrs={'data-header': '姓名'})[0].a.string.strip()
+                if member_cat in comp['members']:
+                    comp['members'][member_cat].append(member_name)
+                else:
+                    comp['members'][member_cat] = []
+                    comp['members'][member_cat].append(member_name)
+        except:
+            print(response.url, '产生异常！！！未找到member。')
+        yield comp
